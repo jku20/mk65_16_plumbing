17e1275e43a2c9b0f028f66c74ef21e8c17cc722
jku20 <jku-ben20@stuy.edu>
2020-12-22 14:58:20 -0500

    realized I missread the assignment so I fixed it

--- console.c
+++ console.c
@@ -7,26 +7,40 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#define BUFFER_SIZE 1024
+#define OUT_BUFFER_SIZE 1024
+//one more than what could come in
+#define IN_BUFFER_SIZE 8192+1
 
 void catch_error(int er);
 
-char buff[BUFFER_SIZE];
+char out_buff[OUT_BUFFER_SIZE];
+char in_buff[IN_BUFFER_SIZE];
 int sucssess;
 
 int main() {
-    //make pipe
+    //make pipes
     sucssess = mkfifo("waluigi",0644);
+    if(errno != EEXIST) catch_error(sucssess);
+
+    sucssess = mkfifo("wario",0644);
     if(errno != EEXIST) catch_error(sucssess);
 
     //open pipe out
     int out_pipe = open("waluigi",O_WRONLY);
     catch_error(out_pipe);
+    //open pipe in
+    int in_pipe = open("wario",O_RDONLY);
+    catch_error(out_pipe);
 
     //pipe open, write stuff
-    while(fgets(buff, sizeof(buff), stdin)) {
-        sucssess = write(out_pipe,buff,sizeof(buff));
+    while(fgets(out_buff, sizeof(out_buff), stdin)) {
+        sucssess = write(out_pipe,out_buff,sizeof(out_buff));
         catch_error(sucssess);
+        //read and print response
+        sucssess = read(in_pipe,in_buff, sizeof(in_buff)-1);
+        catch_error(sucssess);
+        //in_buff is expected to be a null terminated string
+        printf("%s",in_buff);
     }
 
     return 0;
--- processer.c
+++ processer.c
@@ -5,24 +5,32 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
-//we want this buffer to be larger than the other one
-#define BUFFER_SIZE 1024+1
+//one more than what can come in
+#define IN_BUFFER_SIZE 1024+1
+#define OUT_BUFFER_SIZE 8192
 
 void catch_error(int er);
 void process(char *s);
 
-char buff[BUFFER_SIZE];
+char in_buff[IN_BUFFER_SIZE];
+char out_buff[OUT_BUFFER_SIZE];
 int sucssess;
 
 int main() {
     //open pipe in
     int in_pipe = open("waluigi",O_RDONLY);
     catch_error(in_pipe);
+    //open pipe out
+    int out_pipe = open("wario",O_WRONLY);
+    catch_error(out_pipe);
 
     //pipe open
     //read into buffer
-    while(sucssess = read(in_pipe,buff, sizeof(buff)-1) && sucssess != -1) {
-        process(buff);
+    while(sucssess = read(in_pipe,in_buff, sizeof(in_buff)-1) && sucssess != -1) {
+        process(in_buff);
+        //write output buffer
+        sucssess = write(out_pipe,out_buff,sizeof(out_buff));
+        catch_error(sucssess);
     }
     //error on read
     catch_error(sucssess);
@@ -36,6 +44,7 @@ void catch_error(int er) {
         exit(-1);
     }
 }
+//stuffs subsets (or as much as will fit) into the output buffer
 void process(char *s) {
     //remove newline
     char *p = strchr(s,'\n');
@@ -45,15 +54,30 @@ void process(char *s) {
     int n = strlen(s);
 
     char tmp[n];
-    int i,j,c;
-    for(i = 0; i < 1 << n; i++) {
-        c = 0;
+    int i,j;
+    int overal_place = 0;;
+    for(i = 1; i < 1 << n; i++) {
         for(j = 0; j < n; j++) {
             if(i & (1<<j)) {
-                tmp[c++] = s[j];
+                if(overal_place == OUT_BUFFER_SIZE-2){
+                    out_buff[overal_place++] = '\n';
+                    out_buff[overal_place] = '\0';
+                    return;
+                }
+                out_buff[overal_place++] = s[j];
+
             }
         }
-        tmp[c] = '\0';
-        printf("subset %d: %s\n",i+1,tmp);
+        if(i != (1 << n) -1 ) {
+            if(overal_place == OUT_BUFFER_SIZE-2) {
+                out_buff[overal_place++] = '\n';
+                out_buff[overal_place] = '\0';
+                return;
+            }
+            out_buff[overal_place++] = ',' ;
+        }
     }
+    //we know overal_place <= OUT_BUFFER_SIZE-2 as otherwise it would have returned by now
+    out_buff[overal_place++] = '\n';
+    out_buff[overal_place] = '\0';
 }
